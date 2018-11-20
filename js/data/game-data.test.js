80d9b0aa3f941498fd28718cec8acd6048ccaf2c
“Petr <pvvnxdementor5@gmail.com>
2018-11-20 19:16:07 +0300

    fixed the first test description

--- js/data/game-data.test.js
+++ js/data/game-data.test.js
@@ -2,7 +2,7 @@ import {assert} from 'chai';
 
 describe(`Array`, () => {
   describe(`#indexOf()`, () => {
-    it(`should return -1 when the value is present`, () => {
+    it(`should return -1 when the value is not present`, () => {
       assert.equal(-1, [1, 2, 3].indexOf(4));
     });
   });
